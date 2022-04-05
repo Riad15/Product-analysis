2871e0eec33e1e813e1b797a100f261f59a3d827
asif riad <asifriad15@gmail.com>
2022-04-05 21:24:29 +0600

    404 page updated

--- src/component/DefoultPage/Defoultpage.js
+++ src/component/DefoultPage/Defoultpage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './DefoultPage.css'
 
 const Defoultpage = () => {
     return (
         <div>
             <h1>404</h1>
+            <p>this page not found</p>
 
         </div>
     );
